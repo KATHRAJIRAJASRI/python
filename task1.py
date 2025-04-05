16663da357cc605563ef2e500fcd439ac0588586
KATHRAJIRAJASRI <kathrajirajasri@gmail.com>
2025-04-05 10:28:41 +0530

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,3 @@
+a=["continue","hi","hi","hello","break","break","count","count","count","count"]
+b=input("enter the string")
+print(a.count(b))
---
+++ task2.py
@@ -0,0 +1,4 @@
+flowers=["rose","lilly","sunflower"]
+a=input("enter a flower name which you like")
+b=flowers.append(a)
+print(flowers.copy())
