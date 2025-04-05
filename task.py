02faf671c243f6e9b59cbda2dfc8d194ca439f0f
KATHRAJIRAJASRI <kathrajirajasri@gmail.com>
2025-04-05 10:47:31 +0530

    Add files via upload

---
+++ task.py
@@ -0,0 +1,13 @@
+#1.implement the count method on a list by taking input from the user without using the actual method.
+
+a=["continue","hi","hi","hello","break","break","count","count","count","count"]
+b=input("enter the string")
+print(a.count(b))
+
+#2.implement the copy method on a list just as the above condition.
+flowers=["rose","lilly","sunflower"]
+a=input("enter a flower name which you like")
+b=flowers.append(a)
+print(flowers.copy())
+
+
