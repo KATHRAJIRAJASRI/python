766705175d5b0875ac4e0949e6e6810ddf852e73
KATHRAJIRAJASRI <kathrajirajasri@gmail.com>
2025-04-30 23:05:44 +0530

    Add files via upload

--- task1.py
+++ task1.py
@@ -1,24 +1,6 @@
-# program to separate odd & even elements from list 
-list=[1,2,3,4,5,6,7,8,9,10]
-even=[]
-odd=[]
-for i in list:
-    if i%2==0:
-        even.append(i)
-    else:
-        odd.append(i)
-even.extend(odd)
-print(even)
-#program to separate unique elements from list and add "*" at EOL
-a=[2,3,4,5,2,1,3,2]
-b=[]
-c=[]
-for i in a:
-    if i in b:
-        c.append("*")
-    else:
-        b.append(i)
-b.extend(c)
-print(b)
-
+a=5
+for row in range(1,a+1):
+    for col in range(1,a+1):
+        print(str(row),end="")
+    print()
     
