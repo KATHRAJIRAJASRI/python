04eb7e1835d900f380fabd934e01ad351c9a29d5
KATHRAJIRAJASRI <kathrajirajasri@gmail.com>
2025-05-04 16:40:35 +0530

    Add files via upload

---
+++ amstrongandevennum.py
@@ -0,0 +1,28 @@
+# Print the prime digits in the given number
+a=123456
+b=a
+while b>0:
+    last=b%10
+    b=b//10
+    if last%2==0:
+       print(last)
+
+# Practice Armstrong number program
+a=678
+b=a
+temp=a
+len=0
+while a>0:
+      a=a//10
+      len=len+1   
+t=0
+while b>0:
+    last=b%10
+    sq=last**len
+    t=t+sq
+    b=b//10
+if temp==t:
+    print("amstrong")
+else:
+    print("not amstrong")
+
