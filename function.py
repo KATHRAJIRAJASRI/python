852f1b1a55c4a1292a8b8864d8ee2cdad9cdc2b9
KATHRAJIRAJASRI <kathrajirajasri@gmail.com>
2025-05-06 11:12:26 +0530

    Add files via upload

---
+++ function.py
@@ -0,0 +1,31 @@
+# perform basic mathematical operations (+,-,,/,//,%,*) using def function and lambda functions
+#addition
+def addition(n1,n2):
+    return n1+n2
+b=addition(1,2)
+print(b)
+#substraction
+def addition(n1,n2):
+    return n1-n2
+b=addition(1,2)
+print(b)
+#multiplication
+def addition(n1,n2):
+    return n1*n2
+b=addition(1,2)
+print(b)
+#division
+def addition(n1,n2):
+    return n1/n2
+b=addition(1,2)
+print(b)
+#modulus
+def addition(n1,n2):
+    return n1%n2
+b=addition(1,2)
+print(b)
+#floordivision
+def addition(n1,n2):
+    return n1//n2
+b=addition(1,2)
+print(b)
